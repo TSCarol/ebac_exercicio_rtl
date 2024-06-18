fe068ad50f052bf951458b6eaf55265fbe9d7167
TSCarol <thaiscsantoos@gmail.com>
2024-06-18 19:19:22 -0300

    atualização projeto

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,35 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import Post from './index';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+        render(<Post/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    it('Deve adicionar dois comentários corretamente', () => {
+        render(<Post />);
+
+        const textarea = screen.getByTestId('comment-textarea');
+        const submitButton = screen.getByTestId('comment-button');
+
+        fireEvent.change(textarea, {
+            target: {
+                value: 'Primeiro comentário' 
+            }
+        });
+        fireEvent.click(submitButton);
+
+        fireEvent.change(textarea, {
+            target: {
+                value: 'Segundo comentário'
+            }
+        });
+        fireEvent.click(submitButton);
+
+        const comments = screen.getAllByTestId('comment-item');
+        expect(comments).toHaveLength(2);
+        expect(comments[0]).toHaveTextContent('Primeiro comentário');
+        expect(comments[1]).toHaveTextContent('Segundo comentário');
     });
 });
